d07547a8403c50aeff56dfd122205a435c90994a
Aditya <aditya@sublucid.com>
2008-11-27 03:24:05 -0500

    Oops.  Bring back old extensions methods

--- lib/multi_site/user_extensions.rb
+++ lib/multi_site/user_extensions.rb
@@ -1,4 +1,11 @@
 module MultiSite::UserExtensions
+   def self.included(base)
+     base.belongs_to :site
+   end
+   
+   def owner?(multi_site)
+     site == multi_site
+   end
 
   # Remove this from the base User model since we want to scope it by site and
   # ther's no clean way of overriding it.
